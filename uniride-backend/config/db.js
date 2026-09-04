2f5277770ccc4215f98f12c13e2dd438a3f1000e
Sebastián Contreras Lugo <seb4stian53@gmail.com>
2026-09-04 13:23:45 -0600

    conexión db

---
+++ uniride-backend/config/db.js
@@ -0,0 +1,30 @@
+const sql = require('mssql');
+
+const dbSettings = {
+  user: 'admin_db',
+  password: '123456',
+  server: 'localhost',
+  database: 'uniride',
+  port: 1433,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  }
+};
+
+// Crear un pool de conexiones global
+const poolPromise = new sql.ConnectionPool(dbSettings)
+  .connect()
+  .then(pool => {
+    console.log('✅ Conexión a la base de datos SQL Server (uniride) establecida con éxito.');
+    return pool;
+  })
+  .catch(err => {
+    console.error('❌ Error al conectar con la base de datos: ', err);
+    process.exit(1);
+  });
+
+module.exports = {
+  sql,
+  poolPromise
+};
